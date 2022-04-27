4d39ab6b918a007acd6b3e7c59743a1a4825f065
Matt Miller <matthew.c.anthony.miller@gmail.com>
2022-04-27 15:41:51 -0700

    Refactored sale status logic

--- src/actions/getSaleStatus.ts
+++ src/actions/getSaleStatus.ts
@@ -6,32 +6,26 @@ import { SaleStatus } from "../types";
 export const getSaleStatus = async (contract: AirWild2Sale) => {
   const saleStarted = await contract.saleStarted();
   const firstSaleIndex = 0;
-  const secondSaleIndex = 1;
 
-  if (!saleStarted) return SaleStatus.NotStarted;
+  if (!saleStarted) {
+    return SaleStatus.NotStarted;
+  }
 
   const currentBlock = await contract.provider.getBlockNumber();
   const startBlock = await contract.saleStartBlock();
-  const firstDuration = await contract.mintlistDurations(firstSaleIndex);
-  const secondDuration = await contract.mintlistDurations(secondSaleIndex);
 
-  if (ethers.BigNumber.from(currentBlock).gt(startBlock.add(firstDuration))) {
-    const currentMintlist = await contract.currentMerkleRootIndex();
-
-    if (currentMintlist.toNumber() >= 1) {
-      return SaleStatus.PublicSale;
-    }
-
-    const numSold = await contract.domainsSold();
-    const totalForSale = await contract.totalForSale();
-
-    if (totalForSale >= numSold) {
-      return SaleStatus.Ended;
-    }
-  }
-
-  if (ethers.BigNumber.from(currentBlock).gt(startBlock.add(secondDuration))) {
+  const numSold = await contract.domainsSold();
+  const totalForSale = await contract.totalForSale();
+  if (numSold >= totalForSale) {
     return SaleStatus.Ended;
   }
-  return SaleStatus.PrivateSale;
+  const currentMintlist = (await contract.currentMerkleRootIndex()).toNumber();
+  const duration = await contract.mintlistDurations(currentMintlist);
+
+  if (ethers.BigNumber.from(currentBlock).gt(startBlock.add(duration))) {
+    return SaleStatus.Ended;
+  }
+  return currentMintlist == firstSaleIndex
+    ? SaleStatus.PrivateSale
+    : SaleStatus.PublicSale;
 };
